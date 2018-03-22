7f6d1053dd835bb4560fda8a4fafc77fe115b416
QingDengYue <zrl412@163.com>
2018-03-22 18:40:12 +0800

    1.U 更新

--- disconf-web-manager/src/main.js
+++ disconf-web-manager/src/main.js
@@ -3,10 +3,15 @@
 import Vue from 'vue'
 import ElementUi from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+
 import App from './App'
 import router from './router'
+import $ from 'jquery'
+import _ from 'lodash'
 import * as Global from './js/global'
 
+window.$ = window.jQuery = $
+window._ = _
 window.__GLOBAL__ = Global
 Vue.use(ElementUi);
 Vue.config.productionTip = false
