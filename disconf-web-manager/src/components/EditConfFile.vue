1a0555bc8a439b671dc663762fb852b95d08691f
QingDengYue <zrl412@163.com>
2018-03-31 17:57:59 +0800

    1.A 增加ACE编辑器加载 2.A 增加文件编辑模式和列表编辑模式

--- disconf-web-manager/src/components/EditConfFile.vue
+++ disconf-web-manager/src/components/EditConfFile.vue
@@ -1,11 +1,226 @@
 <template>
-  <div></div>
+  <div id="editConfFile">
+    <el-row>
+      <el-col :span="24" style="height: 48px;padding-top: 18px;padding-bottom: 18px;">
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item>首页</el-breadcrumb-item>
+          <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+          <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+          <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+        </el-breadcrumb>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24">
+        <div class="app-statistics-info">
+          <el-row style="height: 100%;line-height: 70px;padding-left: 18px;box-sizing: border-box;">
+            <el-col :span="4">
+              <span class="app-header-label">应用：</span>
+              <span class="app-header-item">winshows_consumer</span>
+            </el-col>
+            <el-col :span="3">
+              <span class="app-header-label">环境配置：</span>
+              <span class="app-header-item">rd</span>
+            </el-col>
+            <el-col :span="3">
+              <span class="app-header-label">配置版本：</span>
+              <span class="app-header-item">1_0_0_0</span>
+            </el-col>
+            <el-col :span="4">
+              <span class="app-header-label">配置名称：</span>
+              <span class="app-header-item">config.properties</span>
+            </el-col>
+            <el-col :span="3">
+              <span class="app-header-label">历史版本：</span>
+              <span class="app-header-item app-header-link">1个</span>
+            </el-col>
+            <el-col :span="3">
+              <span class="app-header-label">配置文件：</span>
+              <span class="app-header-item app-header-link">1个</span>
+            </el-col>
+            <el-col :span="4">
+              <span class="app-header-label">配置项：</span>
+              <span class="app-header-item app-header-link">1个</span>
+            </el-col>
+          </el-row>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row style="margin-top: 10px">
+      <el-col :span="24">
+        <el-col :span="15"><span class="app-message-warnings">正处于编辑状态</span></el-col>
+        <el-col :span="9" style="text-align: right;padding-right: 10px;box-sizing: border-box;">
+          <el-button type="primary" style="margin-left: 10px;" @click="setEditorMode">{{fileEditorName}}</el-button>
+          <el-button type="primary" style="margin-left: 10px;">保存配置</el-button>
+          <el-button type="danger" style="margin-left: 10px;">删除配置</el-button>
+        </el-col>
+      </el-col>
+    </el-row>
+    <el-row v-if="!fileEditorMode" style="margin-top: 10px;">
+      <el-col :span="24">
+        <el-table
+          :data="fileItems"
+          border
+          style="width: 100%">
+          <el-table-column
+            label="Key"
+            width="180">
+            <template slot-scope="scope">
+              <span>{{scope.row.key}}</span>
+              <a class="disconf-icon-copy" :data-clipboard-text="scope.row.key"></a>
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="Value"
+            width="380">
+            <template slot-scope="scope">
+              <span>{{scope.row.value}}</span><a class="disconf-icon-copy" :data-clipboard-text="scope.row.value"></a>
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="说明">
+            <template slot-scope="scope">
+              <span>{{scope.row.comments}}</span><a class="disconf-icon-copy"
+                                                    :data-clipboard-text="scope.row.comments"></a>
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="操作"
+            width="160"
+          >
+            <template slot-scope="scope">
+              <el-button
+                size="mini"
+                icon="el-icon-edit"
+                type="primary"
+                @click="handleEdit(scope.$index, scope.row)">
+              </el-button>
+              <el-button
+                size="mini"
+                icon="el-icon-delete"
+                type="danger"
+                @click="handleDelete(scope.$index, scope.row)">
+              </el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-col>
+    </el-row>
+    <el-row v-if="fileEditorMode" style="margin-top: 10px;">
+      <el-col :span="24">
+        <div class="disconf-ace-editor">
+          <div id="mainEditor">
+
+          </div>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 <script>
+  import {resolve} from '../js/disconf/resolver'
+  import ClipboardJS from 'clipboard'
+  import ACELoader from '../js/ace'
+
   export default {
-    name: 'EditConfFile'
+    name: 'EditConfFile',
+    data() {
+      return {
+        fileEditorMode: false,
+        fileEditorName: '切换至文件模式',
+        fileItems: [],
+        editorInstance: null
+      }
+    },
+    mounted: function () {
+      let originContent = '#权重设置\n' +
+        'hint.key=123\n' +
+        '#网页URL参数\n' +
+        '#Baidu首页地址\n' +
+        'hint.addres=http://www.baidu.com\n'
+      let fileAllItems = resolve(originContent)
+      this.fileItems = fileAllItems
+      this.$nextTick(() => {
+        let vmSelf = this
+        let registerClipboardEvent = new ClipboardJS('.disconf-icon-copy')
+        registerClipboardEvent.on('success', function (e) {
+          if (e.action === 'copy') {
+            vmSelf.$message({
+              message: '已复制',
+              type: 'success'
+            })
+          }
+        })
+      })
+    },
+    methods: {
+      handleEdit(row) {
+
+      },
+      handleDelete(row) {
+
+      },
+      loaderAceEditor() {
+        let vmSelf = this
+        if (this.editorInstance) {
+          this.editorInstance.destroy()
+        }
+        setTimeout(function () {
+          ACELoader.create({
+            el: 'mainEditor',
+            ready: function (editor) {
+              vmSelf.editorInstance = editor
+            }
+          })
+        }, 100)
+      },
+      setEditorMode() {
+        this.fileEditorMode = !this.fileEditorMode
+        this.fileEditorName = this.fileEditorMode ? '切换至列表模式' : '切换至文件模式'
+        this.$nextTick(() => {
+          if (this.fileEditorMode) {
+            this.loaderAceEditor()
+          }
+        })
+      }
+    }
   }
 </script>
 <style lang="stylus">
+  #editConfFile
+    width 100%
+    padding-left 10px
+    padding-right 10px
+    box-sizing border-box
+    .disconf-ace-editor
+      position relative
+      width 100%
+      height 400px
+      #mainEditor
+        position absolute
+        width 100%
+        height 100%
+        min-height 400px
+        border 1px solid #DCDFE6
+    .app-message-warnings
+      color #E6A23C
+    .app-statistics-info
+      height 70px
+      width 100%
+      background-color #F2F2F2
+      span
+        display inline-block
+      .app-header-label
+        font-size 16px
+        display inline-block
+        font-weight bold
+      .app-header-item
+        font-size 14px
+        display inline-block
+      .app-header-link
+        color #409EFF
+        opacity .8
+        cursor pointer
+
 
 </style>
