ad9eb9e25ec8ac0f77c743703bfd39bfe27eea0f
QingDengYue <zrl412@163.com>
2018-04-19 15:02:09 +0800

    1.A 新增用户权限页面

---
+++ disconf-web-manager/src/components/UserPermission.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="userPermission">
+    <page-sequence :data="page" @pageChange="pageChange"></page-sequence>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'UserPermission',
+    data() {
+      return {
+        page: {
+          allCount: 112,
+          pages: [],
+          pageCount: 1,
+          pageNumber: 1,
+          pageSize: 50
+        }
+      }
+    },
+    methods: {
+      pageChange(pageNumber) {
+        console.info(pageNumber)
+      }
+    }
+  }
+</script>
+<style lang="stylus">
+  #userPermission
+    width 100%
+</style>
