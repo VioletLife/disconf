6f83bf021ac7f6c567bdfbdc7c5162a246fd0042
QingDengYue <zrl412@163.com>
2018-04-25 16:33:49 +0800

    1.A 新增部门编辑 部门删除 部门隐藏/显示功能

--- disconf-web-manager/src/components/Department.vue
+++ disconf-web-manager/src/components/Department.vue
@@ -21,7 +21,7 @@
           default-expand-all
           :expand-on-click-node="false">
            <span class="custom-tree-node" slot-scope="{ node, data }">
-        <span>{{ node.label }}</span>
+            <span>{{ data.departmentName }}</span>
         <span>
           <el-button
             type="text"
@@ -45,7 +45,8 @@
             type="text"
             size="mini"
             @click="() => hideDepartment(node, data)">
-            隐藏
+            <template v-if="data.isHiddenNode">显示</template>
+            <template v-if="!data.isHiddenNode">隐藏</template>
           </el-button>
         </span>
       </span>
@@ -55,22 +56,28 @@
     <el-dialog :visible.sync="addOrgDialogVisible" title="新增组织">
       <el-form :model="addOrgForm" :rules="addOrgFormRules" ref="addOrgFormRef">
         <template v-if="isAddDepartmentOperate">
-          <el-form-item label="上级名称：" :label-width="formLabelWidth">
+          <el-form-item label="上级编码：" :label-width="formLabelWidth">
             <span>{{addOrgForm.parentCode}}</span>
           </el-form-item>
           <el-form-item label="上级名称：" :label-width="formLabelWidth">
-            <<span>{{addOrgForm.parentName}}</span>
+            <span>{{addOrgForm.parentName}}</span>
           </el-form-item>
         </template>
         <el-form-item label="部门编码：" :label-width="formLabelWidth" prop="departmentCode">
-          <el-input v-model="addOrgForm.departmentCode" placeholder="请输入部门编码"></el-input>
-          <el-button type="primary" @click="generateCurrentNodeCode">自动生成</el-button>
+          <template v-if="isEditDepartmentOperate">
+            <span>{{addOrgForm.departmentCode}}</span>
+          </template>
+          <template v-if="!isEditDepartmentOperate">
+            <el-input v-model="addOrgForm.departmentCode" placeholder="请输入部门编码" style="width: 80%;"></el-input>
+            <el-button type="primary" @click="generateCurrentNodeCode">自动生成</el-button>
+          </template>
         </el-form-item>
         <el-form-item label="部门名称：" :label-width="formLabelWidth" prop="departmentName">
-          <el-input v-model="addOrgForm.departmentName" placeholder="请输入部门名称"></el-input>
+          <el-input v-model="addOrgForm.departmentName" placeholder="请输入部门名称" style="width: 80%;"></el-input>
         </el-form-item>
         <el-form-item label="部门说明：" :label-width="formLabelWidth">
-          <el-input type="textarea" :rows="2" v-model="addOrgForm.departmentComments" placeholder="请输入部门说明"></el-input>
+          <el-input type="textarea" :rows="2" v-model="addOrgForm.departmentComments" placeholder="请输入部门说明"
+                    style="width: 80%;"></el-input>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -85,9 +92,10 @@
 
   export default {
     name: 'Department',
-    data() {
+    data () {
       return {
         isAddDepartmentOperate: false,
+        isEditDepartmentOperate: false,
         addOrgDialogVisible: false,
         formLabelWidth: '100px',
         addOrgForm: {
@@ -107,106 +115,279 @@
             {min: 1, max: 10, message: '长度在 1 到 20字符', trigger: 'blur'}
           ]
         },
-        departmentData: [
-          {
-            id: 1,
-            label: '神州',
-            children: [
-              {
-                id: 2,
-                label: '神州',
-                children: [
-                  {
-                    id: 3,
-                    label: '神州',
-                    children: [
-                      {
-                        id: 4,
-                        label: '神州',
-                        children: [
-                          {
-                            id: 5,
-                            label: '神州'
-                          }
-                        ]
-                      }
-                    ]
-                  }
-                ]
-              }
-            ]
-          }
-        ]
+        departmentData: [],
+        parentNode: null,
+        currentEditDepartmentId: 0,
+        currentEditDepartmentNode: null
       }
     },
+    mounted () {
+      this.loadDepartmentTree()
+    },
     methods: {
-      loadDepartmentTree() {
+      loadDepartmentTree () {
+        let vmSelf = this
         Utils.ajax({
           url: 'api/org/department/list',
           success: function (response) {
-            console.info(response)
+            vmSelf.departmentData = response.result
           }
         })
       },
-      addOrg() {
+      addOrg () {
         this.setIsAddDepartmentOperate(false)
         this.addOrgDialogVisible = true
       },
-      addDepartment(node, data) {
+      addDepartment (node, data) {
         this.setIsAddDepartmentOperate(true)
+        this.addOrgForm.parentCode = data.departmentCode
+        this.addOrgForm.parentName = data.departmentName
+        this.addOrgDialogVisible = true
+        this.isEditDepartmentOperate = false
       },
-      editDepartment(node, data) {
-        this.setIsAddDepartmentOperate(true)
-      },
-      deleteDepartment(node, data) {
-
-      },
-      hideDepartment(node, data) {
-
-      },
-      closeAddOrgDialog() {
-        this.addOrgDialogVisible = false
-      },
-      resetAddDepartmentDialog() {
-
-      },
-      confirmAddOrgDialog(formName) {
+      editDepartment (node, data) {
+        this.currentEditDepartmentId = data.id
+        this.currentEditDepartmentNode = data
+        if (data.departmentCode.length === 3) {
+          this.setIsAddDepartmentOperate(false)
+        } else {
+          this.setIsAddDepartmentOperate(true)
+        }
+        this.isEditDepartmentOperate = true
+        let currentParentCode = data.parentCode
         let vmSelf = this
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            let departmentForm = {}
-            Utils.ajax({
-              type: 'POST',
-              url: 'api/org/department/create',
-              dataType: 'json',
-              data: JSON.stringify(departmentForm),
-              success: function (response) {
-                if (response && response.message && response.message.status && response.message.status.code && response.message.status.code !== 0) {
-                  vmSelf.$message({
-                    type: 'error',
-                    message: response.message.status.message
-                  })
-                } else {
-                  vmSelf.addRoleDialog = false
-                  vmSelf.resetAddDepartmentDialog()
-                  vmSelf.$message({
-                    type: 'success',
-                    message: '部门添加成功'
-                  })
-                  Utils.sleep(500).then(() => {
-                    vmSelf.loadDepartmentTree()
-                  })
-                }
-              }
-            })
+        this.departmentData.forEach((node) => {
+          if (node.departmentCode === currentParentCode) {
+            vmSelf.parentNode = node
+          } else {
+            vmSelf.lookupParentNode(node.children, currentParentCode)
+          }
+        })
+        if (vmSelf.parentNode) {
+          vmSelf.addOrgForm.parentCode = vmSelf.parentNode.departmentCode
+          vmSelf.addOrgForm.parentName = vmSelf.parentNode.departmentName
+        }
+        vmSelf.addOrgForm.departmentCode = data.departmentCode
+        vmSelf.addOrgForm.departmentName = data.departmentName
+        vmSelf.addOrgForm.departmentComments = data.departmentComments
+        this.addOrgDialogVisible = true
+      },
+      lookupParentNode (children, currentParentCode) {
+        let vmSelf = this
+        children.forEach(function (node) {
+          if (node.departmentCode === currentParentCode) {
+            vmSelf.parentNode = node
+          } else {
+            vmSelf.lookupParentNode(node.children, currentParentCode)
           }
         })
       },
-      setIsAddDepartmentOperate(value) {
+      deleteDepartment (node, data) {
+        let vmSelf = this
+        let h = this.$createElement
+        this.$msgbox({
+          title: '删除确认',
+          message: h('div', null, ['确认删除当前部门(此操作会删除其子节点):', h('span', {style: {color: 'red'}}, [data.departmentName])]),
+          showCancelButton: true,
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          callback: function (action) {
+            if (action === 'confirm') {
+              let departmentId = data.id
+              Utils.ajax({
+                url: 'api/org/department/delete',
+                dataType: 'json',
+                data: {
+                  departmentId
+                },
+                success: function (response) {
+                  if (response && response.message && response.message.status && response.message.status.code && response.message.status.code !== 0) {
+                    vmSelf.$message({
+                      type: 'error',
+                      message: response.message.status.message
+                    })
+                  } else {
+                    vmSelf.addRoleDialog = false
+                    vmSelf.resetAddDepartmentDialog()
+                    vmSelf.$message({
+                      type: 'success',
+                      message: '部门删除成功'
+                    })
+                    vmSelf.closeAddOrgDialog()
+                    Utils.sleep(500).then(() => {
+                      vmSelf.loadDepartmentTree()
+                    })
+                  }
+                }
+              })
+            }
+          }
+        })
+      },
+      hideDepartment (node, data) {
+        let vmSelf = this
+        let commandName = data.isHiddenNode ? '显示' : '隐藏'
+        let h = this.$createElement
+        this.$msgbox({
+          title: commandName + '确认',
+          message: h('div', null, ['确认' + commandName + '当前部门:', h('span', {style: {color: 'red'}}, [data.departmentName])]),
+          showCancelButton: true,
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          callback: function (action) {
+            if (action === 'confirm') {
+              let departmentForm = {
+                id: data.id,
+                departmentCode: data.departmentCode,
+                departmentName: data.departmentName,
+                departmentComments: data.departmentComments,
+                parentCode: data.parentCode,
+                isOrgRoot: data.isOrgRoot,
+                isHiddenNode: !data.isHiddenNode
+              }
+              Utils.ajax({
+                type: 'POST',
+                url: 'api/org/department/update',
+                dataType: 'json',
+                contentType: 'application/json;charset=utf-8',
+                data: JSON.stringify(departmentForm),
+                success: function (response) {
+                  if (response && response.message && response.message.status && response.message.status.code && response.message.status.code !== 0) {
+                    vmSelf.$message({
+                      type: 'error',
+                      message: response.message.status.message
+                    })
+                  } else {
+                    vmSelf.addRoleDialog = false
+                    vmSelf.resetAddDepartmentDialog()
+                    vmSelf.$message({
+                      type: 'success',
+                      message: '部门更新成功'
+                    })
+                    vmSelf.closeAddOrgDialog()
+                    Utils.sleep(500).then(() => {
+                      vmSelf.loadDepartmentTree()
+                    })
+                  }
+                }
+              })
+            }
+          }
+        })
+      },
+      closeAddOrgDialog () {
+        this.addOrgDialogVisible = false
+      },
+      resetAddDepartmentDialog () {
+        this.addOrgForm.parentCode = ''
+        this.addOrgForm.parentName = ''
+        this.addOrgForm.departmentCode = ''
+        this.addOrgForm.departmentName = ''
+        this.addOrgForm.departmentComments = ''
+      },
+      confirmAddOrgDialog (formName) {
+        let vmSelf = this
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            /**
+             * 处于编辑模式
+             */
+            if (vmSelf.isEditDepartmentOperate) {
+              let departmentForm = {
+                id: vmSelf.currentEditDepartmentId,
+                departmentCode: vmSelf.addOrgForm.departmentCode,
+                departmentName: vmSelf.addOrgForm.departmentName,
+                departmentComments: vmSelf.addOrgForm.departmentComments,
+                parentCode: vmSelf.currentEditDepartmentNode.parentCode,
+                isOrgRoot: vmSelf.currentEditDepartmentNode.isOrgRoot,
+                isHiddenNode: vmSelf.currentEditDepartmentNode.isHiddenNode
+              }
+              Utils.ajax({
+                type: 'POST',
+                url: 'api/org/department/update',
+                dataType: 'json',
+                contentType: 'application/json;charset=utf-8',
+                data: JSON.stringify(departmentForm),
+                success: function (response) {
+                  if (response && response.message && response.message.status && response.message.status.code && response.message.status.code !== 0) {
+                    vmSelf.$message({
+                      type: 'error',
+                      message: response.message.status.message
+                    })
+                  } else {
+                    vmSelf.addRoleDialog = false
+                    vmSelf.resetAddDepartmentDialog()
+                    vmSelf.$message({
+                      type: 'success',
+                      message: '部门更新成功'
+                    })
+                    vmSelf.closeAddOrgDialog()
+                    Utils.sleep(500).then(() => {
+                      vmSelf.loadDepartmentTree()
+                    })
+                  }
+                }
+              })
+            } else {
+              let departmentForm = {
+                departmentCode: vmSelf.addOrgForm.departmentCode,
+                departmentName: vmSelf.addOrgForm.departmentName,
+                departmentComments: vmSelf.addOrgForm.departmentComments,
+                parentCode: this.isAddDepartmentOperate ? vmSelf.addOrgForm.parentCode : '',
+                isOrgRoot: !this.isAddDepartmentOperate,
+                isHiddenNode: false
+              }
+              Utils.ajax({
+                type: 'POST',
+                url: 'api/org/department/create',
+                dataType: 'json',
+                contentType: 'application/json;charset=utf-8',
+                data: JSON.stringify(departmentForm),
+                success: function (response) {
+                  if (response && response.message && response.message.status && response.message.status.code && response.message.status.code !== 0) {
+                    vmSelf.$message({
+                      type: 'error',
+                      message: response.message.status.message
+                    })
+                  } else {
+                    vmSelf.addRoleDialog = false
+                    vmSelf.resetAddDepartmentDialog()
+                    vmSelf.$message({
+                      type: 'success',
+                      message: '部门添加成功'
+                    })
+                    vmSelf.closeAddOrgDialog()
+                    Utils.sleep(500).then(() => {
+                      vmSelf.loadDepartmentTree()
+                    })
+                  }
+                }
+              })
+            }
+          }
+        })
+      },
+      setIsAddDepartmentOperate (value) {
         this.isAddDepartmentOperate = value
       },
-      generateCurrentNodeCode() {
-
+      generateCurrentNodeCode () {
+        let parentCode = ''
+        if (this.isAddDepartmentOperate) {
+          parentCode = this.addOrgForm.parentCode
+        } else {
+          parentCode = ''
+        }
+        let vmSelf = this
+        Utils.ajax({
+          url: 'api/org/department/code',
+          dataType: 'json',
+          data: {
+            parentCode
+          },
+          success: function (response) {
+            vmSelf.addOrgForm.departmentCode = response.result
+          }
+        })
       }
     }
   }
