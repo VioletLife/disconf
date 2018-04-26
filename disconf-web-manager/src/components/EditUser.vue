c3c61a9a8dbddd838b87bd79aa3d43cf52687504
QingDengYue <zrl412@163.com>
2018-04-26 17:31:10 +0800

    1.A 新增用户编辑页面

---
+++ disconf-web-manager/src/components/EditUser.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="editUser" class="page-container">
+
+  </div>
+</template>
+<script>
+  export default {
+    name: 'editUser'
+  }
+</script>
+<style lang="stylus">
+  #editUser
+    width 100%
+</style>
