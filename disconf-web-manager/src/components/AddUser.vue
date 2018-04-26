e0e179a9310c13ec9434255f832febd3f7c50046
QingDengYue <zrl412@163.com>
2018-04-26 17:30:54 +0800

    1.A 新增用户添加页面

---
+++ disconf-web-manager/src/components/AddUser.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="addUser" class="page-container">
+
+  </div>
+</template>
+<script>
+  export default {
+    name: 'AddUser'
+  }
+</script>
+<style lang="stylus">
+  #addUser
+    width 100%
+</style>
